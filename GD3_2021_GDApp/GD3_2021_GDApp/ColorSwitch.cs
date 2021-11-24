5cdfd64010283b4ad7ee67bfdc1247ad576bd553
MaddestMax98 <marianocayzac@hotmail.com>
2021-11-24 20:13:22 +0000

    Move all custom methods to custom class file

--- GD3_2021_GDApp/GD3_2021_GDApp/ColorSwitch.cs
+++ GD3_2021_GDApp/GD3_2021_GDApp/ColorSwitch.cs
@@ -23,19 +23,19 @@ namespace ColorSwitchGame
             material.Texture = Content.Load<Texture2D>("Assets/ColorSwitch/Wall/Texture/BrickWall");
             material.Shader = new BasicShader(Application.Content);
 
-            var Wall = new GameObject("cube", GameObjectType.Architecture);
+            var wall = new GameObject("wall", GameObjectType.Architecture);
             var renderer = new MeshRenderer();
             renderer.Material = material;
-            Wall.AddComponent(renderer);
+            wall.AddComponent(renderer);
             renderer.Mesh = new CubeMesh();
 
             #endregion Archetype
 
-            //Roof
+            //Roof 
             for (int i = 0; i < 3; i++)
             {
-                var clone = Wall.Clone() as GameObject;
-                clone.Name = $"{clone.Name} - {1}";
+                var clone = wall.Clone() as GameObject;
+                clone.Name = $"{clone.Name} - {i}";
                 clone.Transform.SetTranslation(0, 40, -10 - (i * 50));
                 clone.Transform.SetScale(0.5f, 50, 50);
                 clone.Transform.SetRotation(0,0 , 90);
@@ -45,8 +45,8 @@ namespace ColorSwitchGame
             //Floor
             for (int i = 0; i < 3; i++)
             {
-                var clone = Wall.Clone() as GameObject;
-                clone.Name = $"{clone.Name} - {1}";
+                var clone = wall.Clone() as GameObject;
+                clone.Name = $"{clone.Name} - {i + 3}";
                 clone.Transform.SetTranslation(0, -40, -10 - (i * 50));
                 clone.Transform.SetScale(0.5f, 50, 50);
                 clone.Transform.SetRotation(0, 0, 90);
@@ -56,8 +56,8 @@ namespace ColorSwitchGame
             //Right wall
             for (int i = 0; i < 3; i++)
             {
-                var clone = Wall.Clone() as GameObject;
-                clone.Name = $"{clone.Name} - {1}";
+                var clone = wall.Clone() as GameObject;
+                clone.Name = $"{clone.Name} - {i + 6}";
                 clone.Transform.SetTranslation(25, 0, -10 - (i*50));
                 clone.Transform.SetScale(0.5f, 80, 50);
                 level.Add(clone);
@@ -66,8 +66,8 @@ namespace ColorSwitchGame
             //Left wall
             for (int i = 0; i < 3; i++)
             {
-                var clone = Wall.Clone() as GameObject;
-                clone.Name = $"{clone.Name} - {1}";
+                var clone = wall.Clone() as GameObject;
+                clone.Name = $"{clone.Name} - {i + 9}";
                 clone.Transform.SetTranslation(-25, 0, -10 - (i * 50));
                 clone.Transform.SetScale(0.5f, 80, 50);
                 level.Add(clone);
@@ -79,31 +79,75 @@ namespace ColorSwitchGame
         {
             #region Archetype
 
-            var material = new BasicMaterial("model material");
-            material.Texture = Content.Load<Texture2D>("Assets/Demo/Textures/checkerboard");
+            var material = new BasicMaterial("simple diffuse");
+            material.Texture = Content.Load<Texture2D>("Assets/ColorSwitch/Wall/Texture/BrickWall");
             material.Shader = new BasicShader(Application.Content);
 
-            var archetypalSphere = new GameObject("sphere", GameObjectType.Consumable);
-            archetypalSphere.IsStatic = false;
-
-            var renderer = new ModelRenderer();
+            var platform = new GameObject("platform", GameObjectType.Architecture);
+            var renderer = new MeshRenderer();
             renderer.Material = material;
-            archetypalSphere.AddComponent(renderer);
-            renderer.Model = Content.Load<Model>("Assets/Models/sphere");
-
-            //downsize the model a little because the sphere is quite large
-            archetypalSphere.Transform.SetScale(0.125f, 0.125f, 0.125f);
+            platform.AddComponent(renderer);
+            renderer.Mesh = new CubeMesh();
 
             #endregion Archetype
 
-            var count = 0;
-            for (var i = -8; i <= 8; i += 2)
-            {
-                var clone = archetypalSphere.Clone() as GameObject;
-                clone.Name = $"{clone.Name} - {count++}";
-                clone.Transform.SetTranslation(-5, i, 0);
-                level.Add(clone);
-            }
+            //Main Platform
+            var clone = platform.Clone() as GameObject;
+            clone.Name = $"{clone.Name} - {1}";
+            clone.Transform.SetTranslation(0, -5, 5);
+            clone.Transform.SetScale(10, 5, 5);
+            level.Add(clone);
+
+        }
+
+        /// <summary>
+        /// Add Platforms to our demo.
+        /// </summary>
+        /// <param name="level"></param>
+        public void InitializePlatforms(Scene level, ContentManager Content)
+        {
+            //For now we can make the level here, we can create a seperate file/class for clarity and simplicity later
+
+            //Initialize all platform types
+
+            //Red stationary platform
+            var rendererRS = new MeshRenderer();
+            rendererRS.Mesh = new CubeMesh();
+            var materialRS = new BasicMaterial("red");
+            materialRS.Texture = Content.Load<Texture2D>("red");
+            materialRS.Shader = new BasicShader(Application.Content);
+            rendererRS.Material = materialRS;
+
+            var platformObjectRS = new GameObject("Platform Stationary Red", GameObjectType.Interactable);
+            platformObjectRS.Transform.SetScale(2, 0.5f, 2);
+            platformObjectRS.AddComponent(rendererRS);
+            var switcherR = new PlatformerSwitch(true);
+            platformObjectRS.AddComponent(switcherR);
+
+            //Blue stationary platform
+            var rendererBS = new MeshRenderer();
+            rendererBS.Mesh = new CubeMesh();
+            var materialBS = new BasicMaterial("blue");
+            materialBS.Texture = Content.Load<Texture2D>("blue");
+            materialBS.Shader = new BasicShader(Application.Content);
+            rendererBS.Material = materialBS;
+
+            var platformObjectBS = new GameObject("Platform Stationary Blue", GameObjectType.Interactable);
+            platformObjectBS.Transform.SetScale(2, 0.5f, 2);
+            platformObjectBS.AddComponent(rendererBS);
+            var switcherB = new PlatformerSwitch(false);
+            platformObjectBS.AddComponent(switcherB);
+
+            //Create Platform clones
+            var platformObjectRSClone = platformObjectRS.Clone() as GameObject;
+            platformObjectRSClone.Transform.SetTranslation(2, 0, 0);
+            platformObjectRSClone.Transform.SetScale(2, 0.5f, 2);
+            level.Add(platformObjectRSClone);
+
+            var platformObjectBSClone = platformObjectBS.Clone() as GameObject;
+            platformObjectBSClone.Transform.SetTranslation(-2, 0, 0);
+            platformObjectBSClone.Transform.SetScale(2, 0.5f, 2);
+            level.Add(platformObjectBSClone);
         }
     }
 }
--- GD3_2021_GDApp/GD3_2021_GDApp/Main.cs
+++ GD3_2021_GDApp/GD3_2021_GDApp/Main.cs
@@ -134,13 +134,13 @@ namespace GDApp
             
             InitializeSkybox(activeScene, 500);
 
-            // ColorSwitch Make walls
+            // ColorSwitch Make walls + Neutral Platforms
             ColorSwitch game = new ColorSwitch();
             game.InitializeBounds(activeScene, Content);
             game.InitializeNeutralPlatforms(activeScene, Content);
 
             //Load platforms
-            InitializePlatforms(activeScene);
+            game.InitializePlatforms(activeScene, Content);
 
             sceneManager.Add(activeScene);
             sceneManager.LoadScene("level 1");
@@ -344,56 +344,6 @@ namespace GDApp
                 clone.Transform.SetScale(1, i, 1);
                 level.Add(clone);
             }
-        }
-
-        /// <summary>
-        /// Add Platforms to our demo.
-        /// </summary>
-        /// <param name="level"></param>
-        private void InitializePlatforms(Scene level)
-        {
-            //For now we can make the level here, we can create a seperate file/class for clarity and simplicity later
-
-            //Initialize all platform types
-
-            //Red stationary platform
-            var rendererRS = new MeshRenderer();
-            rendererRS.Mesh = new CubeMesh();
-            var materialRS = new BasicMaterial("red");
-            materialRS.Texture = Content.Load<Texture2D>("red");
-            materialRS.Shader = new BasicShader(Application.Content);
-            rendererRS.Material = materialRS;
-
-            var platformObjectRS = new GameObject("Platform Stationary Red", GameObjectType.Interactable);
-            platformObjectRS.Transform.SetScale(2, 0.5f, 2);
-            platformObjectRS.AddComponent(rendererRS);
-            var switcherR = new PlatformerSwitch(true);
-            platformObjectRS.AddComponent(switcherR);
-
-            //Blue stationary platform
-            var rendererBS = new MeshRenderer();
-            rendererBS.Mesh = new CubeMesh();
-            var materialBS = new BasicMaterial("blue");
-            materialBS.Texture = Content.Load<Texture2D>("blue");
-            materialBS.Shader = new BasicShader(Application.Content);
-            rendererBS.Material = materialBS;
-
-            var platformObjectBS = new GameObject("Platform Stationary Blue", GameObjectType.Interactable);
-            platformObjectBS.Transform.SetScale(2, 0.5f, 2);
-            platformObjectBS.AddComponent(rendererBS);
-            var switcherB = new PlatformerSwitch(false);
-            platformObjectBS.AddComponent(switcherB);
-
-            //Create Platform clones
-            var platformObjectRSClone = platformObjectRS.Clone() as GameObject;
-            platformObjectRSClone.Transform.SetTranslation(2, 0, 0);
-            platformObjectRSClone.Transform.SetScale(2, 0.5f, 2);
-            level.Add(platformObjectRSClone);
-
-            var platformObjectBSClone = platformObjectBS.Clone() as GameObject;
-            platformObjectBSClone.Transform.SetTranslation(-2, 0, 0);
-            platformObjectBSClone.Transform.SetScale(2, 0.5f, 2);
-            level.Add(platformObjectBSClone);
         }
 
         /// <summary>
