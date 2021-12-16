4d52048bf5201a146714ecb9e6b4fb779e6a2d90
D00243429 <D00243429@student.dkit.ie>
2021-12-16 11:51:33 +0000

    Platforms now move but they are very buggy

---
+++ GD3_2021_GDApp/GD3_2021_GDApp/App/Components/FightGravity.cs
@@ -0,0 +1,27 @@
+﻿using GDLibrary;
+using GDLibrary.Components;
+using GDLibrary.Parameters;
+using JigLibX.Physics;
+using Microsoft.Xna.Framework;
+
+namespace GDApp.App.Components
+{
+    class FightGravity : Behaviour
+    {
+        Collider collider;
+        public override void Awake(GameObject gameObject)
+        { 
+            collider = gameObject.GetComponent<Collider>() as Collider;
+            collider.Body.ApplyGravity = false;
+            collider.Body.SetBodyInvInertia(0f,0f, 0f);
+
+            base.Awake(gameObject);
+        }
+
+        public override void Update()
+        {
+            collider.Body.Velocity = gameObject.GetComponent<CurveBehaviour>().Transform.LocalTranslation;
+        }
+
+    }
+}
